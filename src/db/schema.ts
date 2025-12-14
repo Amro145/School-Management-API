8dc86d5e139a5089561d01a2aab3d27cb79f816d
Amro145 <amroaltayeb14@gmail.com>
2025-12-15 00:39:31 +0200

    SAVE

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,13 +1,21 @@
 import { relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+
+
 export const posts = sqliteTable("posts", {
     id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    userId: integer("userId", { mode: "number" }).notNull().references(() => users.id, { onDelete: "cascade" }),
     title: text("title", { length: 256 }).notNull(),
     content: text("content", { length: 256 }).notNull(),
     timestamp: text("timestamp").notNull().default(sql`CURRENT_TIMESTAMP`),
 })
-export const postRelation = relations(posts, ({ many }) => ({
+
+export const postRelation = relations(posts, ({ one, many }) => ({
+    user: one(users, {
+        fields: [posts.userId],
+        references: [users.id],
+    }),
     comments: many(comments),
 }))
 
@@ -19,6 +27,7 @@ export const comments = sqliteTable("comments", {
     timestamp: text("timestamp").notNull().default(sql`CURRENT_TIMESTAMP`),
 
 })
+
 export const commentRelation = relations(comments, ({ one }) => ({
     post: one(posts, {
         fields: [comments.postId],
