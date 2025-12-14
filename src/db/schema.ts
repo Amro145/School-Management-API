7b6e2a479c96bafde4ff078f642f3922994c5073
Amro145 <amroaltayeb14@gmail.com>
2025-12-14 21:07:01 +0200

    save

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,9 +1,27 @@
-import {sql} from "drizzle-orm";
+import { relations, sql } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
-export const posts = sqliteTable ("posts", {
-    id: integer("id",{mode:"number"}).primaryKey({autoIncrement:true}),
-    title: text("title", {length : 256}).notNull(),
-    content: text("content", {length : 256}).notNull(),
+export const posts = sqliteTable("posts", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    title: text("title", { length: 256 }).notNull(),
+    content: text("content", { length: 256 }).notNull(),
     timestamp: text("timestamp").notNull().default(sql`CURRENT_TIMESTAMP`),
 })
+export const postRelation = relations(posts, ({ many }) => ({
+    comments: many(comments),
+}))
+
+export const comments = sqliteTable("comments", {
+    id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+    postId: integer("post", { mode: "number" }).notNull()
+        .references(() => posts.id, { onDelete: "cascade" }),
+    content: text("content", { length: 256 }).notNull(),
+    timestamp: text("timestamp").notNull().default(sql`CURRENT_TIMESTAMP`),
+
+})
+export const commentRelation = relations(comments, ({ one }) => ({
+    post: one(posts, {
+        fields: [comments.postId],
+        references: [posts.id],
+    })
+}))
